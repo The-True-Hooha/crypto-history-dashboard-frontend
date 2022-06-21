c31720974bd86cbfc8727fecf8876cff2c59f115
David Ogar <owogogahhero@outlook.com>
2022-06-21 15:12:53 +0100

    reafactor: page running

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  }
+}, nextConfig;
